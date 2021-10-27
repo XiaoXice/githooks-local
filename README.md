45d5bf2242f53539c2523d91a22d5fef3dd45a4f
LiangXiao <lexliang@didiglobal.com>
2021-10-27 20:50:01 +0800

    init: 初次提交

---
+++ README.md
@@ -0,0 +1,13 @@
+# githooks-local
+
+项目: [githooks](https://github.com/rycus86/githooks)插件
+
+使用 `githooks --use-core-hookspath` 模式的时候 依旧运行本地的hooks。
+
+## 使用
+
+```bash
+git hooks shared add --global git@github.com:XiaoXice/githooks-local.git
+#or
+git hooks shared add --global https://github.com/XiaoXice/githooks-local.git
+```
---
+++ base-template.sh
@@ -0,0 +1,69 @@
+#!/bin/sh
+process_git_hook() {
+    set_main_variables "$1" || return 1
+    shift 1
+
+    execute_local_hooks_in "$LOCAL_HOOKS_DIR" "$@" || return 1
+}
+
+set_main_variables() {
+    HOOK_NAME="$(basename "$1")"
+    GIT_DIR="$(cd "$(git rev-parse --git-dir)" && pwd)"
+    LOCAL_HOOKS_DIR="${GIT_DIR}/hooks"
+
+    echo "HOOK_NAME: $HOOK_NAME"
+    echo "GIT_DIR: $GIT_DIR"
+    echo "LOCAL_HOOKS_DIR: $LOCAL_HOOKS_DIR"
+}
+
+execute_local_hooks_in() {
+    PARENT="$1"
+    shift
+
+    if [ -d "${PARENT}/${HOOK_NAME}" ]; then
+        for HOOK_FILE in "${PARENT}/${HOOK_NAME}"/*; do
+            execute_hook "$HOOK_FILE" "$@" || return 1
+        done
+    elif [ -f "${PARENT}/${HOOK_NAME}" ]; then
+        execute_hook "${PARENT}/${HOOK_NAME}" "$@" || return 1
+    fi
+}
+
+execute_hook() {
+    HOOK_PATH="$1"
+    shift
+
+    # stop if the file does not exist
+    [ -f "$HOOK_PATH" ] || return 0
+
+    is_auto_generated_by_githooks || return 0
+
+    run_hook_file "$@"
+    return $?
+}
+
+is_auto_generated_by_githooks() {
+    grep "Base Git hook template from" "$HOOK_PATH"
+    if [ $? -eq 0 ]; then
+        return 1
+    else
+        return 0
+    fi
+}
+
+run_hook_file() {
+    if [ -x "$HOOK_PATH" ]; then
+        # Run as an executable file
+        "$HOOK_PATH" "$@"
+        return $?
+
+    elif [ -f "$HOOK_PATH" ]; then
+        # Run as a Shell script
+        sh "$HOOK_PATH" "$@"
+        return $?
+    fi
+
+    return 0
+}
+
+process_git_hook "$@" || exit 1
